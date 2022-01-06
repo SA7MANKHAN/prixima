aa61b2d56f3a7477ad016860d48a0f6f2c3c51a0
Salman <salmanwbillo@gmail.com>
2022-01-06 02:03:56 -0800

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+All the resources are used in this project are free for PERSONAL and COMMERCIAL use. 
+Some of the resource requireq attribution (imaaes downloaded from https://www.freepik.com/, https://www.flaticon.com/)
+ Enjoy ;)
---
+++ js/app.js
@@ -0,0 +1,54 @@
+
+
+$(document).ready(function () {
+    //Owl
+    $('.hero-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        navText: ['PREV', 'NEXT'],
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 7000,
+        responsive: {
+            0: {
+                nav: false,
+            },
+            768: {
+                nav: true,
+            }
+        }
+    })
+
+    $('#projects-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        items: 2,
+        dots: true,
+        smartSpeed: 600,
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2,
+                margin: 8,
+            }
+        }
+    })
+
+    $('.reviews-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        smartSpeed: 900,
+        items: 1,
+        margin: 24,
+        autoplay: true,
+        autoplayTimeout: 4000,
+    })
+});
